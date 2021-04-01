ff499a5587aeef09bb224d888e9abcb635929da9
Michael Baker <michael.a.baker34@gmail.com>
2021-04-01 15:25:43 -0500

    refactor albums migration

--- albums_migration.sql
+++ albums_migration.sql
@@ -3,11 +3,10 @@ USE codeup_test_db;
 DROP TABLE IF EXISTS albums;
 
 CREATE TABLE albums (
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     artist VARCHAR(128) NOT NULL,
     name VARCHAR(128),
     release_date SMALLINT UNSIGNED,
     sales FLOAT(6, 2),
-    genre VARCHAR(128),
-    PRIMARY KEY(id)
+    genre VARCHAR(128)
 );
