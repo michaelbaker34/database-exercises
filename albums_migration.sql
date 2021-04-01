3d7846a7849ef8185f65729b88e084679639b233
Michael Baker <michael.a.baker34@gmail.com>
2021-04-01 16:52:09 -0500

    add unique values to albums

--- albums_migration.sql
+++ albums_migration.sql
@@ -3,10 +3,13 @@ USE codeup_test_db;
 DROP TABLE IF EXISTS albums;
 
 CREATE TABLE albums (
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    artist VARCHAR(128) NOT NULL,
-    name VARCHAR(128),
-    release_date SMALLINT UNSIGNED,
-    sales FLOAT(6, 2),
-    genre VARCHAR(128)
+        id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        artist VARCHAR(128) NOT NULL,
+        name VARCHAR(128),
+        release_date SMALLINT UNSIGNED,
+        sales FLOAT(6, 2),
+        genre VARCHAR(128),
+#         UNIQUE unique_name (name)
 );
+
+ALTER TABLE albums ADD UNIQUE(name, artist);
