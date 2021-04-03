6d40d64ce8b02bcb18afc42712c9714df5b853d6
Michael Baker <michael.a.baker34@gmail.com>
2021-04-02 22:41:25 -0500

    add join/innerjoin query

--- join_exercises.sql
+++ join_exercises.sql
@@ -1,2 +1,9 @@
 USE join_test_db;
 
+# join(inner join) user_name(id) | role_name(id)
+SELECT users.name as user_name,
+       roles.name as role_name
+FROM users
+         JOIN roles
+             ON users.role_id = roles.id;
+
