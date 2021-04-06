0fc963389e74e36f445b463a999f3adb86fdbc7a
Michael Baker <michael.a.baker34@gmail.com>
2021-04-06 10:15:06 -0500

    refactor solutions

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -10,21 +10,47 @@ WHERE employees.hire_date = (
     );
 
 
-SELECT *, COUNT(title)
+# Find all the titles held by all employees with the first name Aamod.
+SELECT title
 FROM titles
 WHERE emp_no IN (
     SELECT emp_no
     FROM employees
     WHERE first_name = 'Aamod'
-)
-GROUP BY title;
+);
 
 
+# Find all the current department managers that are female.
 SELECT first_name, last_name
 FROM employees
 WHERE emp_no IN (
     SELECT emp_no
     FROM dept_manager
     WHERE gender = 'F'
-        AND to_date > now()
+        AND to_date > CURDATE()
     );
+
+
+# Find all the department names that currently have female managers.
+SELECT dept_name
+FROM departments
+WHERE dept_no IN (
+    SELECT dept_no
+    FROM dept_manager
+    WHERE emp_no IN (
+        SELECT emp_no
+        FROM employees
+        WHERE gender = 'F'
+    )
+      AND to_date > NOW()
+);
+
+
+# Find the first and last name of the employee with the highest salary.
+SELECT first_name, last_name FROM employees
+WHERE emp_no = (
+    SELECT emp_no FROM salaries
+    WHERE salary = (
+        SELECT MAX(salary) FROM salaries
+    )
+);
